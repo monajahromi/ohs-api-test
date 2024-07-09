c6f75d38bafdf248d90c59fe80d98c4a84f9cd9f
Mona Jahromi <jahromi.mona66@gmail.com>
2024-07-09 10:48:57 +0330

    feat: check if supplier id is valid added to SupplierService

--- integration-service/src/main/java/com/vgcslabs/ohs/service/SupplierService.java
+++ integration-service/src/main/java/com/vgcslabs/ohs/service/SupplierService.java
@@ -1,8 +1,11 @@
 package com.vgcslabs.ohs.service;
 
 import com.google.protobuf.StringValue;
+import com.vgcslabs.ohs.exception.NotFoundException;
 import com.vgcslabs.supplier.SupplierResponse;
 import com.vgcslabs.supplier.SupplierServiceGrpc;
+import io.grpc.Status;
+import io.grpc.StatusRuntimeException;
 import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.stereotype.Service;
 
@@ -14,7 +17,20 @@ public class SupplierService {
 
     public SupplierResponse getSupplierByPid(String supplierPid) {
         var request = StringValue.newBuilder().setValue(supplierPid).build();
-        return this.supplierClient.getSupplierByPid( request);
+        return supplierClient.getSupplierByPid( request);
+    }
+    public SupplierResponse validateSupplier(String supplierPid) {
+        try {
+            return supplierClient.getSupplierByPid(StringValue.newBuilder().setValue(supplierPid).build());
+        } catch (StatusRuntimeException e) {
+            System.out.println("supplierPid not valid, " + e.getMessage());
+            if (e.getStatus().getCode().equals(Status.Code.UNKNOWN)) {
+                throw new NotFoundException(e.getMessage());
+            } else {
+                throw new RuntimeException(e.getMessage());
+            }
+
+        }
     }
 
 }
